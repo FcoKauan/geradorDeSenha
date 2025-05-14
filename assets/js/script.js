f0670a387ee3350dfac07aa9191eca87eca7d602
Fco Kauan <fcokauanclash@gmail.com>
2025-05-14 12:13:09 -0300

    novas funcionalidades - JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,16 @@
 const btnCriarSenha = document.querySelector("#criar--senha");
 const senhaCriada = document.querySelector("#senha--criada");
 
-// funções
+// novas  funcionalidades 
+const abrirFecharSenhaBtn = document.querySelector("#abrir--criar--senha");
+const criarSenhaContainer = document.querySelector("#senha--options");
+const tamanhoInput = document.querySelector("#tamanho");
+const letrasInput = document.querySelector("#letras");
+const numerosInput = document.querySelector("#numeros");
+const simbolosInput = document.querySelector("#simbolos");
+const copiarSenhaBtn = document.querySelector("#copiar--senha");
 
+// funções
 const getLetraMinuscula = () => {
     // letras minusculas atraves da tabela dos codigos das letras
     return (String.fromCharCode(Math.floor(Math.random() * 26) + 97))
@@ -28,14 +36,27 @@ const gerarSenha = (getLetraMinuscula, getLetraMaiuscula, getNumero, getSimbolos
 
     let senha = "";
 
-    const tamanhoSenha = 10;
+    const tamanhoSenha = tamanhoInput.value;
 
-    const geradores = [
-        getLetraMinuscula,
-        getLetraMaiuscula,
-        getNumero, 
-        getSimbolos
-    ]
+    const geradores = []; 
+
+    if (letrasInput.checked) {
+        geradores.push(getLetraMinuscula, getLetraMaiuscula)
+    }
+
+    if (numerosInput.checked) {
+        geradores.push(getNumero)
+    }
+
+    if (simbolosInput.checked) {
+        geradores.push(getSimbolos)
+    }
+
+    console.log(geradores.length)
+
+    if (geradores.length === 0) {
+        return;
+    }
 
     for (i = 0; i < tamanhoSenha; i = i + geradores.length){
         geradores.forEach(() => {
@@ -60,3 +81,6 @@ btnCriarSenha.addEventListener("click", () => {
         getSimbolos
     );
 });
+abrirFecharSenhaBtn.addEventListener("click", () => {
+    criarSenhaContainer.classList.toggle("hide");
+})
